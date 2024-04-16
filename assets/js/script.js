15f32c000bcb09613e7185a222b524c99e6f96ea
Jarvisismy-copilot <ChelseaJarvs3301@icloud.com>
2024-04-15 18:23:39 -0600

    added song facts js file

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+const songs = [
+    {name: "Even Flow",
+    genre: "Rock",
+    album: "Ten",
+    arist: "Pearl Jam",
+    year: "1991"},
+    
+    {name: "Marooned",
+    genre: "Rock",
+    album: "Division Bell",
+    arist: "Pink Floyd",
+    year: "1994" },
+    
+
+
+
+const songFacts = [
+
+    {fact: " After Brooks & Dunn took 'Neon Moon' to No.1 in 1992, you were hard-pressed to walk into any honky tonk and not see someone line dancing to this absolute hit. Drowning your heartbreak under a tacky neon bar sign never sounded so good.",
+    decade: "1990's"},
+
+    {fact: "Shania Twain had this hit twenty-five years later, this hit still gives people goosebumps when they hear the phrase “Let’s go girls.” The singer claims “It’s everyone’s song and they all take ownership of feeling comfortable in their own skin in a different way.”",
+    decade: "1990's"},
+
+    {fact: "Wu-tang Clan’s 'Cream' was one of the most streamed rap songs out of the 90’s having close to 500 million streams since the original release date, also the beat was produced by the leader RZA who eventually left the group.",
+    decade: "1990's"},
+
+    {fact: "'Big Poppa' was seen as being one of Biggie Smalls biggest songs, he said in an interview that this song reflected his struggles obtaining money to leave the hood scene behind and to him what it means to be truly successful.",
+    decade: "1990's"},
+
+    {fact: "'Lose Yourself' was the inaugural winner of the Grammy Award for best rap song in 2004 and became the first rap song ever to be nominated for song of the year, celebrating it’s excellence in lyricism & also the first rap song to win an Oscar.",
+    decade: "2000's"},
+
+    {fact: "“Toxic” brought Britney Spears back to the pop top 10 for the first time in four years when it looked like her success was fading. The song hooks a listener the first time through and never lets go.",
+    decade: "2000's"},
+
+    {fact: "Upon its release, 'Hey Ya!' by Outkast was the rap-funk-pop behemoth reigned at No. 1 on the Hot 100 for nine weeks and became the first song ever to be downloaded 1 million times on iTunes.",
+    decade: "2000's"},
+
+    {fact: "  ",
+    decade: "2000's"},
+
+
+
+
+    {fact: "  ",
+    decade: "2000's"},
+
+
+    {fact: "  ",
+    decade: "2000's"},
+
+
+    {fact: "  ",
+    decade: "2000's"},
+
+
+
+    {fact: "  ",
+    decade: "2000's"},
+
+
+
+
+
+
+
+]
