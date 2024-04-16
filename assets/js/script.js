a449295fd3271b0a9dc77bebbbdf9375513f7a51
Jarvisismy-copilot <ChelseaJarvs3301@icloud.com>
2024-04-15 19:03:32 -0600

    addeded formattin  song facts js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,155 +1,134 @@
 const songs = [
-    {
-        name: "Even Flow",
-        genre: "Rock",
-        album: "Ten",
-        arist: "Pearl Jam",
-        year: "1991"
-    },
+  {
+    name: "Even Flow",
+    genre: "Rock",
+    album: "Ten",
+    arist: "Pearl Jam",
+    year: "1991",
+  },
 
-    {
-        name: "Marooned",
-        genre: "Rock",
-        album: "Division Bell",
-        arist: "Pink Floyd",
-        year: "1994"
-    },
-
-]
-
-
-
+  {
+    name: "Marooned",
+    genre: "Rock",
+    album: "Division Bell",
+    arist: "Pink Floyd",
+    year: "1994",
+  },
+];
 
 const songFacts = [
+  {
+    fact: " After Brooks & Dunn took 'Neon Moon' to No.1 in 1992, you were hard-pressed to walk into any honky tonk and not see someone line dancing to this absolute hit. Drowning your heartbreak under a tacky neon bar sign never sounded so good.",
+    decade: "1990's",
+  },
 
-    {
-        fact: " After Brooks & Dunn took 'Neon Moon' to No.1 in 1992, you were hard-pressed to walk into any honky tonk and not see someone line dancing to this absolute hit. Drowning your heartbreak under a tacky neon bar sign never sounded so good.",
-        decade: "1990's"
-    },
+  {
+    fact: "Shania Twain had this hit twenty-five years later, this hit still gives people goosebumps when they hear the phrase “Let’s go girls.” The singer claims “It’s everyone’s song and they all take ownership of feeling comfortable in their own skin in a different way.”",
+    decade: "1990's",
+  },
 
-    {
-        fact: "Shania Twain had this hit twenty-five years later, this hit still gives people goosebumps when they hear the phrase “Let’s go girls.” The singer claims “It’s everyone’s song and they all take ownership of feeling comfortable in their own skin in a different way.”",
-        decade: "1990's"
-    },
+  {
+    fact: "Wu-tang Clan’s 'Cream' was one of the most streamed rap songs out of the 90’s having close to 500 million streams since the original release date, also the beat was produced by the leader RZA who eventually left the group.",
+    decade: "1990's",
+  },
 
-    {
-        fact: "Wu-tang Clan’s 'Cream' was one of the most streamed rap songs out of the 90’s having close to 500 million streams since the original release date, also the beat was produced by the leader RZA who eventually left the group.",
-        decade: "1990's"
-    },
+  {
+    fact: "'Big Poppa' was seen as being one of Biggie Smalls biggest songs, he said in an interview that this song reflected his struggles obtaining money to leave the hood scene behind and to him what it means to be truly successful.",
+    decade: "1990's",
+  },
 
-    {
-        fact: "'Big Poppa' was seen as being one of Biggie Smalls biggest songs, he said in an interview that this song reflected his struggles obtaining money to leave the hood scene behind and to him what it means to be truly successful.",
-        decade: "1990's"
-    },
+  {
+    fact: "Alternative Rock and it's sub-genres Grunge and Pop Punk expand in popularity and ironically, explode into the mainstream during the 1990's.",
+    decade: "1990's",
+  },
 
-    {
-        fact: "Alternative Rock and it's sub-genres Grunge and Pop Punk expand in popularity and ironically, explode into the mainstream during the 1990's.",
-        decade: "1990's"
-    },
+  {
+    fact: "'Lose Yourself' was the inaugural winner of the Grammy Award for best rap song in 2004 and became the first rap song ever to be nominated for song of the year, celebrating it’s excellence in lyricism & also the first rap song to win an Oscar.",
+    decade: "2000's",
+  },
 
-    {
-        fact: "'Lose Yourself' was the inaugural winner of the Grammy Award for best rap song in 2004 and became the first rap song ever to be nominated for song of the year, celebrating it’s excellence in lyricism & also the first rap song to win an Oscar.",
-        decade: "2000's"
-    },
+  {
+    fact: "'Toxic' brought Britney Spears back to the pop top 10 for the first time in four years when it looked like her success was fading. The song hooks a listener the first time through and never lets go.",
+    decade: "2000's",
+  },
 
-    {
-        fact: "'Toxic' brought Britney Spears back to the pop top 10 for the first time in four years when it looked like her success was fading. The song hooks a listener the first time through and never lets go.",
-        decade: "2000's"
-    },
+  {
+    fact: "Upon its release, 'Hey Ya!' by Outkast was the rap-funk-pop behemoth reigned at No. 1 on the Hot 100 for nine weeks and became the first song ever to be downloaded 1 million times on iTunes.",
+    decade: "2000's",
+  },
 
-    {
-        fact: "Upon its release, 'Hey Ya!' by Outkast was the rap-funk-pop behemoth reigned at No. 1 on the Hot 100 for nine weeks and became the first song ever to be downloaded 1 million times on iTunes.",
-        decade: "2000's"
-    },
+  {
+    fact: "Rihanna collaborated with Jay-Z on ‘Umbrella’ which wass nominated for Record of the Year and Song of the Year Grammy Awards. It spent 10 weeks at the top of the U.K. pop singles chart, the longest of any single in the decade.",
+    decade: "2000's",
+  },
 
-    {
-        fact: "Rihanna collaborated with Jay-Z on ‘Umbrella’ which wass nominated for Record of the Year and Song of the Year Grammy Awards. It spent 10 weeks at the top of the U.K. pop singles chart, the longest of any single in the decade.",
-        decade: "2000's"
-    },
+  {
+    fact: "If it seems like ‘I Hope You Dance’ has been around forever, it's because Lee Ann Womack's biggest hit is timeless. This song is about taking chances and not being afraid to try something new.",
+    decade: "2000's",
+  },
 
-    {
-        fact: "If it seems like ‘I Hope You Dance’ has been around forever, it's because Lee Ann Womack's biggest hit is timeless. This song is about taking chances and not being afraid to try something new.",
-        decade: "2000's"
-    },
+  {
+    fact: "Dierks Bentley burst onto the country music scene in 2003 when his self-titled debut album's toe-tapping first single climbed to the top of the Billboard country charts.",
+    decade: "2000's",
+  },
 
-    {
-        fact: "Dierks Bentley burst onto the country music scene in 2003 when his self-titled debut album's toe-tapping first single climbed to the top of the Billboard country charts.",
-        decade: "2000's"
-    },
+  {
+    fact: "Florida Georgia Line’s insanely catchy tune ‘Cruise’ spent 24 weeks at number one on the country chart and is the best-selling country digital song of all time in the US, and is generally considered the most famous example of the genre termed 'bro-country'.",
+    decade: "2010's",
+  },
 
-    {
-        fact: "Florida Georgia Line’s insanely catchy tune ‘Cruise’ spent 24 weeks at number one on the country chart and is the best-selling country digital song of all time in the US, and is generally considered the most famous example of the genre termed 'bro-country'.",
-        decade: "2010's"
-    },
+  {
+    fact: "It's an astounding accomplishment to record 30 No. 1 songs but beyond that, Luke Bryan … changed the course of country music. He gave his heart, his lifestyle, his musical infusion, powerful charisma, and Elvis-like moves, & forever changed the sound and reached for a new era in country music.",
+    decade: "2010's",
+  },
 
-    {
-        fact: "It's an astounding accomplishment to record 30 No. 1 songs but beyond that, Luke Bryan … changed the course of country music. He gave his heart, his lifestyle, his musical infusion, powerful charisma, and Elvis-like moves, & forever changed the sound and reached for a new era in country music.",
-        decade: "2010's"
-    },
+  {
+    fact: "Pop-stars ruled the radio stations, focusing their lyrics on drunken nights out with friends, the highlights of love and vengeful heartbreak.",
+    decade: "2010's",
+  },
 
-    {
-        fact: "Pop-stars ruled the radio stations, focusing their lyrics on drunken nights out with friends, the highlights of love and vengeful heartbreak.",
-        decade: "2010's"
-    },
+  {
+    fact: "Noted as one of the most prominent celebrities of the 21st century, Swift is recognized for her versatile musicality, songwriting prowess. ‘Anti-Hero’ quickly became Swift's biggest hit ever, reigning atop the Hot 100 for eight weeks and surpassing ‘Blank Space’ as her longest-running No. 1 song.",
+    decade: "2020's",
+  },
 
-    {
-        fact: "Noted as one of the most prominent celebrities of the 21st century, Swift is recognized for her versatile musicality, songwriting prowess. ‘Anti-Hero’ quickly became Swift's biggest hit ever, reigning atop the Hot 100 for eight weeks and surpassing ‘Blank Space’ as her longest-running No. 1 song.",
-        decade: "2020's"
-    },
+  {
+    fact: "Morgan Wallen’s success can be attributed to his distinctive voice and relatable songwriting which set him apart from other country artists. His ability to connect with listeners on a personal level has resonated with fans and contributed to his success.",
+    decade: "2020's",
+  },
 
-    {
-        fact: "Morgan Wallen’s success can be attributed to his distinctive voice and relatable songwriting which set him apart from other country artists. His ability to connect with listeners on a personal level has resonated with fans and contributed to his success.",
-        decade: "2020's"
-    },
+  {
+    fact: "",
+    decade: "2020's",
+  },
 
+  {
+    fact: "",
+    decade: "2000's",
+  },
 
+  {
+    fact: "",
+    decade: "2000's",
+  },
 
-    {
-        fact: "",
-        decade: "2020's"
-    },
+  {
+    fact: "",
+    decade: "2000's",
+  },
 
-    {
-        fact: "",
-        decade: "2000's"
-    },
+  {
+    fact: "  ",
+    decade: "2000's",
+  },
 
+  {
+    fact: "  ",
+    decade: "2000's",
+  },
 
-
-    {
-        fact: "",
-        decade: "2000's"
-    },
-
-
-    {
-        fact: "",
-        decade: "2000's"
-    },
-
-
-    {
-        fact: "  ",
-        decade: "2000's"
-    },
-
-
-    {
-        fact: "  ",
-        decade: "2000's"
-    },
-
-
-
-    {
-        fact: "  ",
-        decade: "2000's"
-    },
-
-
-
-
-
-
-
-]
+  {
+    fact: "  ",
+    decade: "2000's",
+  },
+];
